567253181240658f00e20cc6cff4bdd24acb3736
Komatsu <KomatsuK@SI.EDU>
2020-02-06 17:35:36 -0500

    Intro code.

---
+++ APR_darkdiv_sppaccum.R
@@ -0,0 +1,21 @@
+################################################################################
+##  APR_darkdiv_sppaccum.R: Species accumulation curves for Dark Diversity Network plots at APR.
+##
+##  Author: Kimberly Komatsu
+##  Date created: February 5, 2020
+################################################################################
+
+library(tidyverse)
+
+setwd('C:\\Users\\komatsuk\\Dropbox (Smithsonian)\\American Prairie Reserve\\APR_darkdiv') #desktop
+
+
+#read in data
+spp <- read.csv('DarkDivNet_Species_Comp_Traits_2019_forAnalysis.csv')
+
+# TODO - all by bison vs cattle
+# percent native vs introduced
+# diversity by native vs introduced
+# species accumulation curves
+# NMDS
+
